13ad8aac485675aea3aaaafcd589ff4c19cc0919
bastienwcs <email>
2019-06-11 09:23:46 +0200

    init

---
+++ src/main/java/fr/wildcodeschool/seeknwild/SeeknwildApplication.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.seeknwild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeknwildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeknwildApplication.class, args);
+	}
+
+}
