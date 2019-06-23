944cb4c48650f4f2852d8ace85bec08784fbdfc4
Episkey <paul.beziat66@gmail.com>
2019-06-23 14:56:39 +0200

    modif branchUserC

--- src/main/java/fr/wildcodeschool/seeknwild/Controller/UserController.java
+++ src/main/java/fr/wildcodeschool/seeknwild/Controller/UserController.java
@@ -1,8 +1,52 @@
 package fr.wildcodeschool.seeknwild.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import fr.wildcodeschool.seeknwild.Model.User;
+import fr.wildcodeschool.seeknwild.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/user")
+    public List<User> read( ) {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("user/{userId}")
+    public User read(@PathVariable Long userId) {
+        return userRepository.findById(userId).get();
+    }
+
+    @PostMapping("/user")
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/user/{id}")
+    public User update(@PathVariable Long userId, @RequestBody User user) {
+        User userToUpdate = userRepository.findById(userId).get();
+        if (user.getPseudo() != null) {
+            userToUpdate.setPseudo(user.getPseudo());
+        }
+        if (user.getPassword() != null) {
+            userToUpdate.setPassword(user.getPassword());
+        }
+        if (user.getEmail() != null) {
+            userToUpdate.setEmail(user.getEmail());
+        }
+        return userRepository.save(userToUpdate);
+    }
+
+    @DeleteMapping("/user/{userId}")
+    public void delete(@PathVariable Long userId) {
+        userRepository.deleteById(userId);
+    }
+
 
 }
