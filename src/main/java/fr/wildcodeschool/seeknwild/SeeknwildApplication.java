4461da095d40197a77ecb12c615fc2638c3d067c
bastienwcs <email>
2019-07-02 18:22:40 +0200

    release

--- src/main/java/fr/wildcodeschool/seeknwild/SeeknwildApplication.java
+++ src/main/java/fr/wildcodeschool/seeknwild/SeeknwildApplication.java
@@ -2,9 +2,10 @@ package fr.wildcodeschool.seeknwild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SeeknwildApplication {
+public class SeeknwildApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeeknwildApplication.class, args);
