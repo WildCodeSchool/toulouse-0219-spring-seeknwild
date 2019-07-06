8557e0b266039961f89371ee37b0e6d0cee3da44
Episkey <paul.beziat66@gmail.com>
2019-07-06 13:04:54 +0200

    récup dev

--- src/main/java/fr/wildcodeschool/seeknwild/Repository/UserRepository.java
+++ src/main/java/fr/wildcodeschool/seeknwild/Repository/UserRepository.java
@@ -1,6 +1,7 @@
 package fr.wildcodeschool.seeknwild.Repository;
 
 import fr.wildcodeschool.seeknwild.Model.User;
+import fr.wildcodeschool.seeknwild.Model.UserAdventure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,4 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserByEmail(String text);
-
 }
